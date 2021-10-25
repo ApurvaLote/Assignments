305fb4004fd5f429e998a1df2e21c4ef8ea8dd87
ApurvaLote <92042505+ApurvaLote@users.noreply.github.com>
2021-10-25 14:33:42 +0530

    Add files via upload

---
+++ Assignment 2/Pallindrome.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+int isPalindrome(char *str);
+
+int main()
+{
+	char text[50]={0};
+	char ch;	    
+	int  cnt=0;  
+	
+	printf("\n Enter the String to check whether the string is pallindrome or not\t: ");
+	while(1)
+	{
+		ch=getchar();
+		
+		if((ch>='A' && ch<='Z')||(ch>='a' && ch<='z'))
+			text[cnt++]=ch;
+		else
+			break; 
+	}
+	text[cnt]='\0';	
+	
+	
+	printf("\n Entered string is: %s\n",text);
+	
+	if(isPalindrome(text))
+		printf("\n %s is a palindrome string\n",text);
+	else
+		printf("\n %s is not a palindrome string\n",text);
+	
+	return 0;
+}
+
+
+int isPalindrome(char *str)
+{
+	int length=0;
+	int i,ret=1;
+	
+	while(str[length]!='\0') length++;
+	 
+    for(i=0; i<length; i++)
+	{
+		if((str[0+i] != str[(length-1)-i]))
+		{
+			ret=0;
+			break;
+		}			
+	} 	
+	return ret;
+}
+
+
+/*
+
+OUTPUT:
+
+
+ Enter the String to check whether the string is pallindrome or not     : madam
+
+ Entered string is: madam
+
+ madam is a palindrome string
+
+ a.exe
+
+ Enter the String to check whether the string is pallindrome or not     : datastructure
+
+ Entered string is: datastructure
+
+ datastructure is not a palindrome string
+
+
+*/
